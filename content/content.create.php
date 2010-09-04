2e9d7b7295f165e066589ba5771a6bf989e44c13
Creative Dutchmen <hkeemink@gmail.com>
2010-09-04 23:54:29 +0200

    Fixed various bugs

--- content/content.create.php
+++ content/content.create.php
@@ -10,6 +10,101 @@ class contentExtensionUpload_Manycreate extends AdministrationPage {
 	}
 	
 	function __actionNew(){
+
+		$sectionManager = new SectionManager($this->_Parent);
+
+		if(!$section_id = $sectionManager->fetchIDFromHandle($this->_context[1])){
+			$return["error"] = "Section handle is incorrect";
+		}
+		elseif(!$section = $sectionManager->fetch($section_id)){
+			$return["error"] = "Section can not be found.";
+		}
+		else{
+
+			$entryManager = new EntryManager($this->_Parent);
+			$fieldManager = new FieldManager($this->_Parent);
+
+			$entry =& $entryManager->create();
+			$entry->set('section_id', $section_id);
+			$entry->set('author_id', $this->_Parent->Author->get('id'));
+			$entry->set('creation_date', DateTimeObj::get('Y-m-d H:i:s'));
+			$entry->set('creation_date_gmt', DateTimeObj::getGMT('Y-m-d H:i:s'));
+
+			$fields = $_POST['fields'];
+			
+			//$_FILES['fields'][$upload_many_field_name] = $_FILES['file'];
+			
+			## Combine FILES and POST arrays, indexed by their custom field handles
+			if(isset($_FILES['fields'])){
+				$filedata = General::processFilePostData($_FILES['fields']);
+
+				foreach($filedata as $handle => $data){
+					if(!isset($fields[$handle])) $fields[$handle] = $data;
+					elseif(isset($data['error']) && $data['error'] == 4) $fields['handle'] = NULL;
+					else{
+
+						foreach($data as $ii => $d){
+							if(isset($d['error']) && $d['error'] == 4) $fields[$handle][$ii] = NULL;
+							elseif(is_array($d) && !empty($d)){
+
+								foreach($d as $key => $val)
+									$fields[$handle][$ii][$key] = $val;
+							}
+						}
+					}
+				}
+			}
+			
+			if(__ENTRY_FIELD_ERROR__ == $entry->checkPostData($fields, $this->_errors)){
+				
+				$this->pageAlert(__('Some errors were encountered while attempting to save.'), Alert::ERROR);
+				foreach($this->_errors as $fieldId => $error){
+					$errors[$fieldId]['fieldName'] = $fieldManager->fetch($fieldId)->get('element_name');
+					$errors[$fieldId]['error'] = $error;
+				}
+				$return["error"] = $errors;
+			}
+			elseif(__ENTRY_OK__ != $entry->setDataFromPost($fields, $error)){
+				$this->pageAlert($error['message'], Alert::ERROR);
+				$return["error"] = "Something went wrong";
+			}
+			else{
+
+				###
+				# Delegate: EntryPreCreate
+				# Description: Just prior to creation of an Entry. Entry object and fields are provided
+				$this->_Parent->ExtensionManager->notifyMembers('EntryPreCreate', '/publish/new/', array('section' => $section, 'fields' => &$fields, 'entry' => &$entry));
+
+				if(!$entry->commit()){
+					define_safe('__SYM_DB_INSERT_FAILED__', true);
+					$this->pageAlert(NULL, Alert::ERROR);
+					$return["error"] = "Something went wrong in the database";
+
+				}
+
+				else{
+
+					###
+					# Delegate: EntryPostCreate
+					# Description: Creation of an Entry. New Entry object is provided.
+					$this->_Parent->ExtensionManager->notifyMembers('EntryPostCreate', '/publish/new/', array('section' => $section, 'entry' => $entry, 'fields' => $fields));
+
+					$prepopulate_field_id = $prepopulate_value = NULL;
+					if(isset($_POST['prepopulate'])){
+						$prepopulate_field_id = array_shift(array_keys($_POST['prepopulate']));
+						$prepopulate_value = stripslashes(rawurldecode(array_shift($_POST['prepopulate'])));
+					}
+					$return["error"] = "";
+				}
+			}
+		}
+
+		echo json_encode($return);
+		exit();
+	}
+
+	
+	function __actionUpload(){
 		
 		// Settings
 		$targetDir = TMP . DIRECTORY_SEPARATOR . "plupload";
@@ -38,7 +133,6 @@ class contentExtensionUpload_Manycreate extends AdministrationPage {
 
 				// Remove temp files if they are older than the max age
 				if (is_file($filePath) && (substr($file, -2) != 'db') && (filemtime($filePath) < time() - $maxFileAge)){
-					echo $filePath;
 					unlink($filePath);
 				}
 			}
--- fields/field.upload_many.php
+++ fields/field.upload_many.php
@@ -34,7 +34,34 @@ Class fieldUpload_Many extends fieldUpload{
 	}
 	
 	function processRawFieldData($data, &$message, $entry_id=NULL){
+		$data = '/'.$data;
+		if(is_file(TMP . '/plupload' . $data)){
+		
+			$abs_path = DOCROOT . '/' . trim($this->get('destination'), '/');
+			$rel_path = str_replace('/workspace', '', $this->get('destination'));
+			
+			rename(TMP . '/plupload/' . $data, $abs_path . $data);
+			
+			$data = $rel_path . $data;
+			
+		}
 		return parent::processRawFieldData($data, $message, $entry_id);
+	}
+	
+	function checkPostFieldData($data, &$message, $entry_id=NULL){
+	
+		$data = '/'.$data;
+
+		if(file_exists(DOCROOT . '/' . trim($this->get('destination'), "/") . $data)){
+			$message = __('A file with the name %1$s already exists in %2$s. Please rename the file first, or choose another.', array(substr($data,1), $this->get('destination')));
+			return self::__INVALID_FIELDS__;				
+		}
+		
+		//TODO: add validation rules. The normal rules do not apply, because the file has not yet been moved.
+		//maybe this can be solved by adding the moving-logic into this function.
+		
+		return self::__OK__;
+		//return parent::checkPostFieldData($data, &$message, $entry_id);
 	}
 	
 }
